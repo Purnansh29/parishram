a856867fa96bb054b1d3d52b1773039c93f4b096
Purnansh29 <patelpurnansh29@gmail.com>
2026-04-24 10:55:33 +0530

    chore: update imports to match manual folder structure change

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
-import Navbar from '../components/LandingPage/Navbar';
-import HeroSection from '../components/LandingPage/HeroSection';
-import FeaturesSection from '../components/LandingPage/FeaturesSection';
-import Footer from '../components/LandingPage/Footer';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import Footer from '../components/Footer';
 
 const LandingPage = () => {
   return (
